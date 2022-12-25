bbd72a371a6a17afb31f47d4dda93f776ffe9a8e
Asad <asad200ana@gmail.com>
2022-12-25 20:49:43 +0530

    bitboard pop and count

--- defs.h
+++ defs.h
@@ -213,6 +213,9 @@ typedef struct
 // MACROS
 
 #define FR2SQ(f, r) ((21 + (f)) + ((r)*10))
+#define SQ64(sq120) Sq120ToSq64[sq120]
+#define POP(b) PopBit(b)
+#define CNT(b) CountBits(b)
 
 // GLOBALS
 
@@ -221,8 +224,12 @@ extern int Sq64ToSq120[64];
 
 // FUNCTIONS
 
+// init.c
 extern void AllInit();
 
-// init.c
+// bitboards.c
+extern void PrintBitBoard(U64 bb);
+extern int PopBit(U64 *bb);
+extern int CountBits(U64 b);
 
 #endif
--- vice.c
+++ vice.c
@@ -5,5 +5,19 @@ int main()
 {
     AllInit();
 
+    U64 playBitBoard = 0ULL;
+
+    playBitBoard |= (1ULL << SQ64(D2));
+    playBitBoard |= (1ULL << SQ64(D3));
+    playBitBoard |= (1ULL << SQ64(D4));
+
+    int sq64 = 0;
+    while (playBitBoard)
+    {
+        sq64 = POP(&playBitBoard);
+        printf("popped:%d\n", sq64);
+        PrintBitBoard(playBitBoard);
+    }
+
     return 0;
 }
